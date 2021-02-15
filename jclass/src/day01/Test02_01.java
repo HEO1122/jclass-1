e4bfa5bdb4a4da97fbab8f80ead8f92e9366f78a
class02 <class02@Class02T01.mshome.net>
2021-02-15 16:44:39 +0900

    day01 1st commit

---
+++ jclass/src/day01/Test02_01.java
@@ -0,0 +1,23 @@
+package day01;
+
+class Test02 {
+	public static void main(String[] args) {
+		/*
+		 * 여기는 여러줄 주석
+		 */
+		
+		// 여기는 한줄 주석...
+	}
+}
+
+class Test02_01 {
+	public static void main(String[] args) {
+		
+	}
+}
+
+class Test02_02 {
+	public static void main(String[] args) {
+		
+	}
+}
