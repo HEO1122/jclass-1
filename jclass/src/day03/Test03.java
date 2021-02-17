a4bac5348c32615fd416540feffe962020d7f07b
class02 <class02@Class02T01>
2021-02-17 12:06:31 +0900

    day03 1st commit

---
+++ jclass/src/day03/Test03.java
@@ -0,0 +1,17 @@
+package day03;
+
+/*
+	문제 ]
+		정수를 입력받아서
+		짝수이면 "짝수입니다."
+		홀수이면 "홀수입니다."
+		
+	(옵션)	0이면	 "0입니다."
+		
+		가 출력되게 프로그램을 작성하세요.
+ */
+public class Test03 {
+	public static void main(String[] args) {
+		
+	}
+}
