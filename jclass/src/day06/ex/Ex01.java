db1f060ecebecdf1be5617b158455410e8f00841
class02 <class02@Class02T01>
2021-02-23 11:08:47 +0900

    day07 1st commit

--- jclass/src/day06/ex/Ex01.java
+++ jclass/src/day06/ex/Ex01.java
@@ -6,6 +6,10 @@ package day06.ex;
 		1 + 1 + 2 + 1 + 2 + 3 + 1 + 2 + 3 + 4 + ... + 19 + 20
 		
 		의 더한 결과를 출력하는 프로그램을 작성하세요.
+		
+		힌트 ]
+			
+			1 + (1 + 2) + (1 + 2 + 3) + (1 + 2 + 3 + 4) + .....
  */
 public class Ex01 {
 	public static void main(String[] args) {
---
+++ jclass/src/day07/ex/Ex01.java
@@ -0,0 +1,17 @@
+package day07.ex;
+
+/*
+	문제 1 ]
+		
+		정수 10개를 기억할 배열을 만들고
+		랜덤하게 1~ 50 사이의 정수를 발생시켜서 입력하고
+		발생된 수 중 제일 작은 값을 출력하는 프로그램을 작성하세요.
+		
+ */
+public class Ex01 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
