3c1d82edf2f54bb4599eec24063202bfa836db8c
class02 <class02@Class02T01>
2021-02-26 16:51:23 +0900

    day10 7th commit

---
+++ jclass/src/day10/ex/Ex05.java
@@ -0,0 +1,22 @@
+package day10.ex;
+
+/*
+
+문제 5 ]
+	두 수를 이용해서 더하는 프로그램을 작성하세요.
+		
+		1. 랜덤한 숫자를 반환해주는 함수
+		2. 숫자 두개를 입력하면 더해준 값을 반환해주는 함수
+		3. 출력해주는 함수
+		
+	세개의 만든 함수를 이용해서 출력하세요.
+ */
+public class Ex05 {
+	public Ex05() {
+		
+	}
+	public static void main(String[] args) {
+		new Ex05();
+	}
+
+}
