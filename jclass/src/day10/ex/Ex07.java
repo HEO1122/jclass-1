d1d5b85a17e7bc39bd6c3782f357a3b271ee4089
class02 <class02@Class02T01>
2021-02-26 18:20:22 +0900

    day10 final commit

---
+++ jclass/src/day10/ex/Ex07.java
@@ -0,0 +1,18 @@
+package day10.ex;
+
+/*
+	문제 7 ]
+		
+		두 정수와 연산기호(+, -, *, /, %) 문자를 입력하면
+		해당 연산의 결과를 반환해주는 함수(calc) 를 만들어서
+		실행하고 출력하세요.
+ */
+public class Ex07 {
+	public Ex07() {
+		
+	}
+	public static void main(String[] args) {
+		new Ex07();
+	}
+
+}
