659430ffffdd7ffa045540a5252728f4287a9593
class02 <class02@Class02T01>
2021-03-02 14:52:40 +0900

    day11 mid commit

---
+++ jclass/src/day11/ex/Ex02.java
@@ -0,0 +1,20 @@
+package day11.ex;
+
+/*
+	문제 2 ]
+		원의 정보를 기억할 클래스(Circle)을 정의하고
+		이 클래스를 10개를 관리할 배열을 만들어서
+			반지름, 둘레, 넓이
+		를 출력하세요. 
+ */
+public class Ex02 {
+	
+	public Ex02() {
+		
+	}
+	
+	public static void main(String[] args) {
+		new Ex02();
+	}
+
+}
