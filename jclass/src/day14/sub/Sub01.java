7f42bcf0d25cee653f638790ff9e642d3645d72d
class02 <class02@Class02T01>
2021-03-05 15:24:02 +0900

    day14 mid commit

---
+++ jclass/src/day14/sub/Sub01.java
@@ -0,0 +1,17 @@
+package day14.sub;
+
+class Sub01 {
+	private int no = 10;
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	// 여기까지가 변수의 은닉화...
+	
+	public Sub01() {
+		System.out.println("*** 여기는 Sub01 기본 생성자 ***");
+	}
+
+}
